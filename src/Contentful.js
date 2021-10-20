dd27910066853710cf02e3ad08999e7792c1445a
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-10-20 12:00:32 +0200

    feat: add enviroment variables

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful'
 
 export default createClient({
-    space:'4sutfq8mypud',
-    accessToken:'M_r4i--Ib8LgOUqpWFGTGjAut0x_4uPyw3gXG2R97wk'
+    space:process.env.REACT_APP_SPACE,
+    accessToken:process.env.REACT_APP_ACCESS_TOKEN
 })
